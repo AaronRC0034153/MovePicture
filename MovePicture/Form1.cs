ba515fa25dcb10444124011d66d275ae1893f6c3
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-11-20 10:16:01 +0000

    added picture and 4 buttons

--- MovePicture/Form1.cs
+++ MovePicture/Form1.cs
@@ -16,5 +16,10 @@ namespace MovePicture
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
