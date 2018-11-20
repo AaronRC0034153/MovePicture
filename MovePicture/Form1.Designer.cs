1e6850798db3c81ed94c2f53b78ab27e17209bdb
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-11-20 10:27:23 +0000

    aligned the buttons

--- MovePicture/Form1.Designer.cs
+++ MovePicture/Form1.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(547, 185);
+            this.button1.Location = new System.Drawing.Point(632, 386);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -59,7 +59,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(538, 109);
+            this.button2.Location = new System.Drawing.Point(632, 415);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -69,7 +69,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(415, 91);
+            this.button3.Location = new System.Drawing.Point(551, 415);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -79,7 +79,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(441, 271);
+            this.button4.Location = new System.Drawing.Point(713, 415);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
